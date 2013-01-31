f8188f11e7df813e306e6a160d59f7cc676891c5
Stephan Ruttloff <stephan@ruttloff.org>
2013-01-31 18:03:53 +0100

    changed everything to use yahoo codes and icons

--- KindleWeatherStationProvider/KindleWeatherStationProvider.py
+++ KindleWeatherStationProvider/KindleWeatherStationProvider.py
@@ -9,58 +9,132 @@ from xml.dom import minidom
 import datetime
 import codecs
 
-
-
 #
 # Download and parse weather data
 #
 
 # Fetch data (change lat and lon to desired location)
-weather_xml = urllib2.urlopen('http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=39.3286&lon=-76.6169&format=24+hourly&numDays=4&Unit=m').read()
+weather_xml = urllib2.urlopen('http://weather.yahooapis.com/forecastrss?w=643134&u=c').read()
 dom = minidom.parseString(weather_xml)
 
-# Parse temperatures
-xml_temperatures = dom.getElementsByTagName('temperature')
-highs = [None]*4
-lows = [None]*4
-for item in xml_temperatures:
-    if item.getAttribute('type') == 'maximum':
-        values = item.getElementsByTagName('value')
-        for i in range(len(values)):
-            highs[i] = int(values[i].firstChild.nodeValue)
-    if item.getAttribute('type') == 'minimum':
-        values = item.getElementsByTagName('value')
-        for i in range(len(values)):
-            lows[i] = int(values[i].firstChild.nodeValue)
+yweather = 'http://xml.weather.yahoo.com/ns/rss/1.0'
 
-# Parse icons
-xml_icons = dom.getElementsByTagName('icon-link')
-icons = [None]*4
-for i in range(len(xml_icons)):
-    icons[i] = xml_icons[i].firstChild.nodeValue.split('/')[-1].split('.')[0].rstrip('0123456789')
+city = dom.getElementsByTagNameNS(yweather, 'location')[0].getAttributeNode('city').nodeValue
 
-# Parse dates
-xml_day_one = dom.getElementsByTagName('start-valid-time')[0].firstChild.nodeValue[0:10]
-day_one = datetime.datetime.strptime(xml_day_one, '%Y-%m-%d')
+unit_temp   = dom.getElementsByTagNameNS(yweather, 'units')[0].getAttributeNode('temperature').nodeValue
+unit_dist   = dom.getElementsByTagNameNS(yweather, 'units')[0].getAttributeNode('distance').nodeValue
+unit_prsr   = dom.getElementsByTagNameNS(yweather, 'units')[0].getAttributeNode('pressure').nodeValue
+unit_speed  = dom.getElementsByTagNameNS(yweather, 'units')[0].getAttributeNode('speed').nodeValue
 
+condition_text  = dom.getElementsByTagNameNS(yweather, 'condition')[0].getAttributeNode('text').nodeValue
+condition_code  = dom.getElementsByTagNameNS(yweather, 'condition')[0].getAttributeNode('code').nodeValue
+condition_temp  = dom.getElementsByTagNameNS(yweather, 'condition')[0].getAttributeNode('temp').nodeValue
+condition_date  = dom.getElementsByTagNameNS(yweather, 'condition')[0].getAttributeNode('date').nodeValue
 
+forecasts   = dom.getElementsByTagNameNS(yweather, 'forecast')
 
-#
-# Preprocess SVG
-#
+forecast_day    = [None]*2
+forecast_date   = [None]*2
+forecast_low    = [None]*2
+forecast_high   = [None]*2
+forecast_text   = [None]*2
+forecast_code   = [None]*2
 
-# Open SVG to process
+for i in range(len(forecasts)):
+    forecast_day[i]     = forecasts[i].getAttributeNode('day').nodeValue
+    forecast_date[i]    = forecasts[i].getAttributeNode('date').nodeValue
+    forecast_low[i]     = forecasts[i].getAttributeNode('low').nodeValue
+    forecast_high[i]    = forecasts[i].getAttributeNode('high').nodeValue
+    forecast_text[i]    = forecasts[i].getAttributeNode('text').nodeValue
+    forecast_code[i]    = forecasts[i].getAttributeNode('code').nodeValue
+
 output = codecs.open('weather-script-preprocess.svg', 'r', encoding='utf-8').read()
 
-# Insert icons and temperatures
-output = output.replace('ICON_ONE',icons[0]).replace('ICON_TWO',icons[1]).replace('ICON_THREE',icons[2]).replace('ICON_FOUR',icons[3])
-output = output.replace('HIGH_ONE',str(highs[0])).replace('HIGH_TWO',str(highs[1])).replace('HIGH_THREE',str(highs[2])).replace('HIGH_FOUR',str(highs[3]))
-output = output.replace('LOW_ONE',str(lows[0])).replace('LOW_TWO',str(lows[1])).replace('LOW_THREE',str(lows[2])).replace('LOW_FOUR',str(lows[3]))
+output = output.replace('CITY_NAME',city)
+output = output.replace('UNIT_TEMP',unit_temp)
 
-# Insert days of week
-one_day = datetime.timedelta(days=1)
-days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-output = output.replace('DAY_THREE',days_of_week[(day_one + 2*one_day).weekday()]).replace('DAY_FOUR',days_of_week[(day_one + 3*one_day).weekday()])
+icons = [None]*49
+icons[0] = "ra"                 #tornado
+icons[1] = "ra"                 #tropical storm
+icons[2] = "ra"                 #hurricane
+icons[3] = "tsra"               #severe thunderstorms
+icons[4] = "tsra"               #thunderstorms
+icons[5] = "rasn"               #mixed rain and snow
+icons[6] = "ip"                 #mixed rain and sleet
+icons[7] = "mix"                #mixed snow and sleet
+icons[8] = "shra"               #freezing drizzle
+icons[9] = "shra"               #drizzle
+icons[10] = "freezing_rain"     #freezing rain
+icons[11] = "ra"                #showers
+icons[12] = "ra"                #showers
+icons[13] = "snow"              #snow flurries
+icons[14] = "rasn"              #light snow showers
+icons[15] = "blizzard"          #blowing snow
+icons[16] = "snow"              #snow
+icons[17] = "ip"                #hail
+icons[18] = "ip"                #sleet
+icons[19] = "dust"              #dust
+icons[20] = "foggy"             #foggy
+icons[21] = "foggy"             #haze
+icons[22] = "foggy"             #smoky
+icons[23] = "wind"              #blustery
+icons[24] = "wind"              #windy
+icons[25] = "cold"              #cold
+icons[26] = "ovc"               #cloudy
+icons[27] = "cloudy"            #mostly cloudy (night)
+icons[28] = "cloudy"            #mostly cloudy (day)
+icons[29] = "sct"               #partly cloudy (night)
+icons[30] = "sct"               #partly cloudy (day)
+icons[31] = "clear_night"       #clear (night)
+icons[32] = "skc"               #sunny
+icons[33] = "sunny_few_clouds"  #fair (night)
+icons[34] = "sunny_few_clouds"  #fair (day)
+icons[35] = "raip"              #mixed rain and hail
+icons[36] = "hot"               #hot
+icons[37] = "tsra"              #isolated thunderstorms
+icons[38] = "scttsra"           #scattered thunderstorms
+icons[39] = "scttsra"           #scattered thunderstorms
+icons[40] = "hi_shwrs"          #scattered showers
+icons[41] = "snow"              #heavy snow
+icons[42] = "rasn"              #scattered snow showers
+icons[43] = "snow"              #heavy snow
+icons[44] = "sunny_few_clouds"  #partly cloudy
+icons[45] = "tsra"              #thundershowers
+icons[46] = "snow"              #snow showers
+icons[47] = "tsra"              #isolated thundershowers
+icons[48] = "unknown"           #not available (3200)
 
-# Write output
+if (int(condition_code) < 0) or (int(condition_code) > 47):
+    condition_icon = icons[48]
+else:
+    condition_icon = icons[int(condition_code)]
+
+forecast_icon = [None]*2
+
+if (int(forecast_code[0]) < 0) or (int(forecast_code[0]) > 47):
+    forecast_icon[0] = icons[48]
+else:
+    forecast_icon[0] = icons[int(forecast_code[0])]
+
+if (int(forecast_code[1]) < 0) or (int(forecast_code[1]) > 47):
+    forecast_icon[1] = icons[48]
+else:
+    forecast_icon[1] = icons[int(forecast_code[1])]
+
+# AKTUELL
+output = output.replace('CURRENT_TMP',condition_temp)
+output = output.replace('ICON_ONE',condition_icon)
+
+# MORGEN
+output = output.replace('DAY_TWO',forecast_day[0])
+output = output.replace('LOW_TWO',forecast_low[0])
+output = output.replace('HIGH_TWO',forecast_high[0])
+output = output.replace('ICON_TWO',forecast_icon[0])
+
+# UEBERMORGEN
+output = output.replace('DAY_THREE',forecast_day[1])
+output = output.replace('LOW_THREE',forecast_low[1])
+output = output.replace('HIGH_THREE',forecast_high[1])
+output = output.replace('ICON_THREE',forecast_icon[1])
+
 codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
